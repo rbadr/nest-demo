11c28f44b8965b2150429db58ee61114b0f1475e
Badr Rahal <badr.rahal@payfit.com>
2020-10-04 00:41:02 +0200

    fix: ci and tests 🐛(#3)

--- src/controllers/app.spec.ts
+++ src/controllers/app.spec.ts
@@ -1,21 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing'
+
 import { AppController } from './app'
 import { AppService } from '../services/app'
 
 describe('AppController', () => {
   let app: TestingModule
+  const port = 7000
+  const configServiceMockFactory = () => ({ get: jest.fn(() => port) })
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: 'ConfigService',
+          useFactory: configServiceMockFactory,
+        },
+      ],
     }).compile()
   })
 
   describe('getHello', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello from port"', () => {
       const appController = app.get<AppController>(AppController)
-      expect(appController.getHello()).toBe('Hello World!')
+      expect(appController.getHello()).toBe(`Hello from port ${port}`)
     })
   })
 })
